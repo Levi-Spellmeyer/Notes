c031a4ec78b4a0925ec75875ed23b101b7d50d9a
Levi Spellmeyer <levi.spellmeyer.04@gmail.com>
2024-11-22 20:57:47 -0600

    Create Python mega course.py and added notes

---
+++ Python mega course.py
@@ -0,0 +1,8 @@
+# NOTE: for a list of python modules use sys.builtin_module_names
+import time #required module
+time.sleep() #seconds goes in between the ()
+# In the scenario where data is constantly taken in or a constant while loop time.sleep() will pause an interation for specified amount of time
+
+
+
+
